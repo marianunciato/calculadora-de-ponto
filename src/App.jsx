e619eed5c483e3524bd38e84c1a9150e7fe565ba
Maria <maria.eduarda@ftdata.com.br>
2026-08-14 09:52:31 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  	return (
+    	<>
+      		<section className="hero">
+        		<div className="hero__content">
+        
+        		</div>
+      		</section>
+    	</>
+  	)
+}
+
+export default App
